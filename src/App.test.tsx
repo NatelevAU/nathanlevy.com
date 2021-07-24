38cf396c1a497a509fb00b94e2e5277c81d6ebd4
Nathan <nathan.levy.au@gmail.com>
2021-07-24 21:24:12 +1000

    changed yarn build and yarn test, modified test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders source code link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/source code/i);
   expect(linkElement).toBeInTheDocument();
 });
