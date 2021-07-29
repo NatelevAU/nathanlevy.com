455f7deac0c07d826ff750ee862f2a7ad10fd78b
Nathan <nathan.levy.au@gmail.com>
2021-07-29 23:27:35 +1000

    page folders now capitalised

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './pages/home/Home';
+import Home from './pages/Home/Home';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 import './index.css';
