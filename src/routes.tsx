f34b2fe5a218e5548e1b1ee84c350ad3cd2db9ee
Nathan <nathan.levy.au@gmail.com>
2021-07-30 23:04:23 +1000

    built sitemap

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+
+export default (
+  <Switch>
+    <Route path="/home" />
+    <Route path="/spin" />
+  </Switch>
+);
---
+++ src/sitemap-builder.js
@@ -0,0 +1,12 @@
+require('babel-register')({
+  presets: ['es2015', 'react'],
+});
+
+const router = require('./routes.tsx').default;
+const Sitemap = require('react-router-sitemap').default;
+
+function generateSitemap() {
+  return new Sitemap(router).build('https://www.nathanlevy.com').save('./public/sitemap.xml');
+}
+
+generateSitemap();
