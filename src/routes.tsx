e9cb3613580e2f7e418c191a2610ba518b18ee26
Nathan <nathan.levy.au@gmail.com>
2021-08-06 23:32:47 +1000

    removing /home from sitemap

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
-export default (
-  <Switch>
-    <Route path="/home" />
-  </Switch>
-);
+export default <Switch></Switch>;
