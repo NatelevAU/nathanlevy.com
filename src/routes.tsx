eb0a4cad8477a8014e0eee5d2321879e9b3217b2
NatelevAU <nathan.levy.au@gmail.com>
2023-03-16 17:04:01 +1100

    upgraded react-router to v6.9

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 export default (
-  <Switch>
+  <Routes>
     <Route path="/dice" />
-  </Switch>
+  </Routes>
 );
