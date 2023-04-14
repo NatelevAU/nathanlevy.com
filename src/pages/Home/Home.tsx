ced3a70776b02b086c0b2620594b160c242e86e1
NatelevAU <nathan.levy.au@gmail.com>
2023-04-14 02:18:33 +0000

    added social media buttons to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Button, IconButton } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
-import { Link } from 'react-router-dom';
 
 import './Home.css';
 import background from '../../images/backgrounds/Home.jpg';
@@ -14,7 +16,8 @@ const sectionStyle = {
 };
 
 const Home: React.FC<{}> = () => {
-  const SnakeLink: React.FC<{}> = props => <Link to="/snake" {...props} />;
+  const [subtitleIndex, setSubtitleIndex] = useState(0);
+  const subtitles = ['Software Engineer', 'Web Developer', 'Dog Lover'];
 
   const SourceLink: React.FC<{}> = props => (
     <a
@@ -25,21 +28,63 @@ const Home: React.FC<{}> = () => {
     />
   );
 
+  const Mailto = (email: string) => {
+    window.location.href = `mailto:${email}`;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSubtitleIndex((subtitleIndex + 1) % subtitles.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [subtitleIndex, subtitles.length]);
+
   ReactGA.pageview(window.location.pathname);
 
   return (
     <div className="Home" style={sectionStyle}>
       <header className="Home-header">
-        <h1> Coming soon... </h1>
+        <h1> Nathan Levy </h1>
+        <h2> {subtitles[subtitleIndex]} </h2>
         <Button
           color="primary"
           size="large"
           variant="contained"
           sx={{ margin: 2 }}
-          component={SourceLink}
+          href="https://github.com/NatelevAU/natelev"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Source Code
         </Button>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <IconButton
+            color="primary"
+            sx={{ margin: 1 }}
+            href="https://github.com/NatelevAU"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon />
+          </IconButton>
+          <IconButton
+            color="primary"
+            sx={{ margin: 1 }}
+            href="https://www.linkedin.com/in/nathan-levy-au"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            color="primary"
+            sx={{ margin: 1 }}
+            onClick={() => Mailto('nathan.levy.au@gmail.com')}
+          >
+            <EmailIcon />
+          </IconButton>
+        </div>
       </header>
     </div>
   );
