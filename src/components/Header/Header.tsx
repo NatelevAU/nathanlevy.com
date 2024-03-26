cc3fab05e6b7c5541f01d6df90a44573e3f9c1ac
NatelevAU <nathan.levy.au@gmail.com>
2024-03-27 00:17:07 +1100

    added my own updated header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,60 @@
+import Header from '@natelev/header';
+import * as React from 'react';
+
+import { PageConfig } from '../../config/PagesConfigTypes';
+import MenuButton from './MenuButton';
+
+interface TopHeaderProps {
+  leftPages?: PageConfig[];
+  middlePages?: PageConfig[];
+  rightPages?: PageConfig[];
+  logo?: React.FC;
+  background?: string;
+  transparent?: boolean;
+}
+
+const TopHeader: React.FC<TopHeaderProps> = ({
+  leftPages = [],
+  middlePages = [],
+  rightPages = [],
+  logo,
+  background,
+  transparent,
+}) => {
+  const firstElements = leftPages.map(page => {
+    return function FirstElementComponent(props: any) {
+      return <MenuButton {...props} key={page.name} page={page} />;
+    };
+  });
+
+  const centerElements = middlePages.map(page => {
+    return function CenterElementComponent(props: any) {
+      return <MenuButton {...props} key={page.name} page={page} />;
+    };
+  });
+
+  const lastElements = rightPages.map(page => {
+    return function LastElementComponent(props: any) {
+      return <MenuButton {...props} key={page.name} page={page} />;
+    };
+  });
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <Header
+        logo={logo}
+        firstElements={firstElements}
+        middleElements={centerElements}
+        lastElements={lastElements}
+        backgroundColor={transparent || background ? 'transparent' : 'primary.main'}
+        background={transparent ? undefined : background}
+        textColor="white"
+        highlightBackgroundColor="white"
+        highlightTextColor="primary.main"
+        headerType="top"
+      />
+    </div>
+  );
+};
+
+export default TopHeader;
