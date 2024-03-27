9a6f851f7b9bc9a5dc851dd59d708bfb341041fa
NatelevAU <nathan.levy.au@gmail.com>
2024-03-27 18:37:10 +1100

    improved home page by adding workstation image, making icon buttons white, implementing CustomIconButton component to make code cleaner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,41 @@
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Box, IconButton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 
+import CustomIconButton from '../components/CustomIconButton';
 import { emailAddress, githubUrl, linkedinUrl } from '../config/Config';
 
+import workstationImage from '../assets/images/workstation.png';
+
+const Mailto = (email: string) => {
+  window.location.href = `mailto:${email}`;
+};
+
 const Home: React.FC<{}> = () => {
+  ReactGA.pageview(window.location.pathname);
+
   const [subtitleIndex, setSubtitleIndex] = useState(0);
   const subtitles = ['Software Engineer', 'Web Developer', 'Dog Lover'];
-
-  const Mailto = (email: string) => {
-    window.location.href = `mailto:${email}`;
-  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setSubtitleIndex((subtitleIndex + 1) % subtitles.length);
-    }, 3000);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, [subtitleIndex, subtitles.length]);
 
-  ReactGA.pageview(window.location.pathname);
-
   return (
-    <Box sx={{ paddingBottom: 5 }}>
+    <div style={{ justifyContent: 'center' }}>
       <h1> Nathan Levy </h1>
+      <img src={workstationImage} style={{ height: '30vmin', margin: 0, padding: 0 }} />
       <h2> {subtitles[subtitleIndex]} </h2>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <IconButton sx={{ margin: 1 }} href={githubUrl} target="_blank" rel="noopener noreferrer">
-          <GitHubIcon />
-        </IconButton>
-        <IconButton sx={{ margin: 1 }} href={linkedinUrl} target="_blank" rel="noopener noreferrer">
-          <LinkedInIcon />
-        </IconButton>
-        <IconButton sx={{ margin: 1 }} onClick={() => Mailto(emailAddress)}>
-          <EmailIcon />
-        </IconButton>
-      </Box>
-    </Box>
+      <CustomIconButton IconComponent={GitHubIcon} url={githubUrl} />
+      <CustomIconButton IconComponent={LinkedInIcon} url={linkedinUrl} />
+      <CustomIconButton IconComponent={EmailIcon} onClick={() => Mailto(emailAddress)} />
+    </div>
   );
 };
 
