4bbe0f1921b243e633712a53d0f050bda7344717
NatelevAU <nathan.levy.au@gmail.com>
2026-04-04 21:30:21 +1100

    added i18n english translation

---
+++ src/i18n/types.d.ts
@@ -0,0 +1,10 @@
+import enTranslation from './locales/en/translation.json';
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'translation';
+    resources: {
+      translation: typeof enTranslation;
+    };
+  }
+}
